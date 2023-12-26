bbff5775502295143817d24d0ddba0df632a7c6b
SunlightLuck <hope4wd88@gmail.com>
2023-12-26 09:46:00 -0500

    update

--- src/Near/Wallet.test.ts
+++ src/Near/Wallet.test.ts
@@ -77,30 +77,24 @@ describe('basic wallet', () => {
     expect(wallet.getMnemonic()).toBeDefined()
   })
 
-  // const keepixTokens: any = {
-  //       coins: {
-  //           "ethereum": {
-  //               "nativeCoinName": "ETH",
-  //               "name": "Ethereum",
-  //               "type": "evm",
-  //               "icon": "./icons/ETH.png",
-  //               "rpcs": [
-  //                   {
-  //                       "url": "https://mainnet.infura.io/v3/00e69497300347a38e75c3287621cb16",
-  //                       "chainId": 1
-  //                   }
-  //               ],
-  //               "getPriceByPoolBalance": {
-  //                   "tokenA": "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
-  //                   "tokenB": "0x6B175474E89094C44Da98b954EedeAC495271d0F",
-  //                   "tokenADecimals": 18,
-  //                   "tokenBDecimals": 18,
-  //                   "poolAddress": "0xA478c2975Ab1Ea89e8196811F51A7B7Ade33eB11",
-  //                   "blockchain": "ethereum"
-  //               }
-  //           }
-  //       }
-  //   };
+  it('can getTokenInformation', async () => {
+    const mnemonic =
+      'crazy disorder menu senior smooth mask apology shock draw chunk question vital'
+
+    const wallet = new Wallet()
+
+    await wallet.init({
+      networkId: 'testnet',
+      type: 'near',
+      rpc: 'https://rpc.testnet.near.org',
+      mnemonic,
+    })
+    expect(
+      await wallet.getTokenInformation(
+        'laycon.41c17b3a8dfac477986atokenlauncher.testnet',
+      ),
+    ).toEqual({ name: 'Laycon', symbol: 'LAY', decimals: 18 })
+  })
 
   it('can getBalance', async () => {
     const mnemonic =
